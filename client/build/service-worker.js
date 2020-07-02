44e90ff82b6d2218425a4d4084a96502dca77b29
Arthur Aliev <xenonhammer46@gmail.com>
2020-07-03 01:44:59 +0400

    настройка сервера

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c092a2663065be301bd43b1164ca260.js"
+  "/precache-manifest.78ad03cb9463c56d1887ec4dadb36282.js"
 );
 
 self.addEventListener('message', (event) => {
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const path = require('path')
 const express = require('express')
-
-
 const app = express();
+
+
+const PORT = 4000;
+const baseUrl = 'http://localhost:4000'
 
 app.use('/api/parse', require('./routes/parse.routes'))
 
-const PORT = 4000;
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, ()=>console.log(`Сервер запущен. Ипользуется порт: ${PORT}`))
